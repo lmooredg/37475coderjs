82342e4f64982f7889f36d8abb366d2a11824a9d
lmooredg <70295354+lmooredg@users.noreply.github.com>
2022-06-24 18:55:13 -0300

    Primer desafío entregable

---
+++ 03 - Desafio Entregable/algoritmo.js
@@ -0,0 +1,8 @@
+let i = parseInt(prompt("Ingresar un número menor a 20 para sumar de a 2"));
+do {
+	i = i + 2;
+	alert ("La suma da "+i+" y si le sumo 2 más")
+	} 
+while(i<=20) {
+	alert ("hasta aquí llegamos. Adiós!")
+	}
